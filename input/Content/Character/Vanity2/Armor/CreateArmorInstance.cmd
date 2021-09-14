05ae7c0c0fa3be5f8c452625dcc4b37c52578d04
DarthPointer <al.kiriackin@yandex.ru>
2021-09-15 00:56:29 +0300

    Restored Automation Core

---
+++ input/Content/Character/Vanity2/Armor/CreateArmorInstance.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+CALL DVP_DAUM_AUTOMATION_New_Armor_Instance_Config
+
+COPY SK_Vanity_ArmorDriller002.uasset SK_Vanity_%ArmorName%.uasset
+COPY SK_Vanity_ArmorDriller002.uexp SK_Vanity_%ArmorName%.uexp
+
+IF %UseBeard%==TRUE (
+	COPY SK_Vanity_ArmorDriller002_Beard.uasset SK_Vanity_%ArmorName%_Beard.uasset
+	COPY SK_Vanity_ArmorDriller002_Beard.uexp SK_Vanity_%ArmorName%_Beard.uexp
+	)
+	
+COPY SK_Vanity_ArmorDriller002_FP.uasset SK_Vanity_%ArmorName%_FP.uasset
+COPY SK_Vanity_ArmorDriller002_FP.uexp SK_Vanity_%ArmorName%_FP.uexp
+
+COPY VAN_ArmorDriller_MK2.uasset VAN_%ArmorName%.uasset
+COPY VAN_ArmorDriller_MK2.uexp VAN_%ArmorName%.uexp
+
+COPY Paintjobs_ArmorDefaults\VAN_ArmorMat_Driller_MK2Default.uasset Paintjobs_ArmorDefaults\VAN_ArmorMat_%ArmorName%_Default.uasset
+COPY Paintjobs_ArmorDefaults\VAN_ArmorMat_Driller_MK2Default.uexp Paintjobs_ArmorDefaults\VAN_ArmorMat_%ArmorName%_Default.uexp
+
+CALL GenerateLocalDS > DVP_DAUM_AUTOMATION_Local.ds
+
+..\..\..\..\..\DAUM\daum.exe -s DVP_DAUM_AUTOMATION_Local.ds
+
+CALL GenerateMergingDS > ..\_VanitySets\ArmorMerging_%ArmorName%.ds
+
+SET ArmorName=
+SET ClassName=
+SET UseBeard=
+
+SET UserVisibleName=
+SET UserVisibleDesc=
+
+SET UserVisibleNameGUID=
+SET UserVisibleDescBUID=
+
+SET ProgressionGUID=
---
+++ input/Content/Character/Vanity2/Armor/GenerateLocalDS.cmd
@@ -0,0 +1,59 @@
+
+
+echo File SK_Vanity_%ArmorName%.uasset
+
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002 /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%
+echo Name -r SK_Vanity_ArmorDriller002 SK_Vanity_%ArmorName%
+
+
+IF %UseBeard%==TRUE (
+	echo File SK_Vanity_%ArmorName%_Beard.uasset
+	
+	echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_Beard /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_Beard
+	echo Name -r SK_Vanity_ArmorDriller002_Beard SK_Vanity_%ArmorName%_Beard
+	)
+
+echo File SK_Vanity_%ArmorName%_FP.uasset
+
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_FP /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_FP
+echo Name -r SK_Vanity_ArmorDriller002_FP SK_Vanity_%ArmorName%_FP
+
+
+echo File VAN_%ArmorName%.uasset
+
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002 /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002.SK_Vanity_ArmorDriller002 /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%.SK_Vanity_%ArmorName%
+
+
+IF %UseBeard%==TRUE (
+	echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_Beard /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_Beard
+	echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_Beard.SK_Vanity_ArmorDriller002_Beard /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_Beard.SK_Vanity_%ArmorName%_Beard
+) ELSE (
+	echo ExportChange -d -i 1 BeardCompatibleMesh ""
+)
+@echo off
+
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_FP /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_FP
+echo Name -r /Game/Character/Vanity2/Armor/SK_Vanity_ArmorDriller002_FP.SK_Vanity_ArmorDriller002_FP /Game/Character/Vanity2/Armor/SK_Vanity_%ArmorName%_FP.SK_Vanity_%ArmorName%_FP
+
+echo Name -r /Game/Character/Vanity2/Armor/VAN_ArmorDriller_MK2 /Game/Character/Vanity2/Armor/VAN_%ArmorName%
+echo Name -r VAN_ArmorDriller_MK2 VAN_%ArmorName%
+
+echo Name -r /Game/Character/Vanity2/Armor/Paintjobs_ArmorDefaults/VAN_ArmorMat_Driller_MK2Default /Game/Character/Vanity2/Armor/Paintjobs_ArmorDefaults/VAN_ArmorMat_%ArmorName%_Default
+echo Name -r VAN_ArmorMat_Driller_MK2Default VAN_ArmorMat_%ArmorName%_Default
+
+echo ExportChange -r -i 1 ItemName/TextProperty/0/9/SPNTS/0 %UserVisibleNameGUID%
+echo ExportChange -r -i 1 ItemName/TextProperty/0/46/SPNTS/0 "%UserVisibleName%"
+
+echo ExportChange -r -i 1 ItemDescription/TextProperty/0/9/SPNTS/0 %UserVisibleDescGUID%
+echo ExportChange -r -i 1 ItemDescription/TextProperty/0/46/SPNTS/0 "%UserVisibleDesc%"
+
+echo ExportChange -r -i 1 SaveGameID/GUID/1 %ProgressionGUID%
+
+echo File Paintjobs_ArmorDefaults/VAN_ArmorMat_%ArmorName%_Default.uasset
+
+echo Name -r /Game/Character/Vanity2/Armor/Paintjobs_ArmorDefaults/VAN_ArmorMat_Driller_MK2Default /Game/Character/Vanity2/Armor/Paintjobs_ArmorDefaults/VAN_ArmorMat_%ArmorName%_Default
+echo Name -r VAN_ArmorMat_Driller_MK2Default VAN_ArmorMat_%ArmorName%_Default
+
+echo Name -r /Game/Character/Vanity2/Armor/VAN_ArmorDriller_MK2 /Game/Character/Vanity2/Armor/VAN_%ArmorName%
+echo Name -r VAN_ArmorDriller_MK2 VAN_%ArmorName%
