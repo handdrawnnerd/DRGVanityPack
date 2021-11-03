b3d84b89d87bc9b8a4bace89a93e9fd490d0cfc1
DarthPointer <al.kiriackin@yandex.ru>
2021-11-03 12:36:01 +0300

    Fixed Eyebrows Merging Config Used Headwear Folder

--- input/Content/Character/Vanity2/Eyebrows/GenerateMergingDS.cmd
+++ input/Content/Character/Vanity2/Eyebrows/GenerateMergingDS.cmd
@@ -2,11 +2,11 @@
 
 echo File VanitySet_Driller.uasset
 
-echo Name -a /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName%
+echo Name -a /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName%
 echo Name -a VAN_Eyebrows_%InternalEyebrowsName%
 
-echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0
-echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
 
 echo ExportChange -a -i 1 EyebrowsItems/Array/0 ""
 echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%InternalEyebrowsName% 0"
@@ -14,11 +14,11 @@ echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%
 
 echo File VanitySet_Engineer.uasset
 
-echo Name -a /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName%
+echo Name -a /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName%
 echo Name -a VAN_Eyebrows_%InternalEyebrowsName%
 
-echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0
-echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
 
 echo ExportChange -a -i 1 EyebrowsItems/Array/0 ""
 echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%InternalEyebrowsName% 0"
@@ -26,11 +26,11 @@ echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%
 
 echo File VanitySet_Gunner.uasset
 
-echo Name -a /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName%
+echo Name -a /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName%
 echo Name -a VAN_Eyebrows_%InternalEyebrowsName%
 
-echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0
-echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
 
 echo ExportChange -a -i 1 EyebrowsItems/Array/0 ""
 echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%InternalEyebrowsName% 0"
@@ -38,11 +38,11 @@ echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%
 
 echo File VanitySet_Scout.uasset
 
-echo Name -a /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName%
+echo Name -a /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName%
 echo Name -a VAN_Eyebrows_%InternalEyebrowsName%
 
-echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0
-echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Headwear/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/CoreUObject Package -i 0 /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0
+echo Import -a /Script/FSD EyeBrowsVanityItem /Game/Character/Vanity2/Eyebrows/VAN_Eyebrows_%InternalEyebrowsName% 0 VAN_Eyebrows_%InternalEyebrowsName% 0
 
 echo ExportChange -a -i 1 EyebrowsItems/Array/0 ""
 echo ExportChange -r -i 1 EyebrowsItems/Array/0/-1/ObjectIndex/0 "VAN_Eyebrows_%InternalEyebrowsName% 0"
