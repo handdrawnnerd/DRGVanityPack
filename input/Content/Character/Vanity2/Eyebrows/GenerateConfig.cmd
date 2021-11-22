d8c4f9bbaf17590f6c9fa3c8d6e8b2cab7e3acd8
handdrawnnerd <msfuhrma@mtu.edu>
2021-11-22 00:10:44 -0500

    Matching latest changes for newest set of cosmetics
    For the PAYDAY/MC/Thankgiving related batch of cosmetics

---
+++ input/Content/Character/Vanity2/Eyebrows/GenerateConfig.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+echo REM NEVER USE SPACES IN THE 2 NAMES BELOW!
+echo REM Leave no space after "="
+
+echo REM Internal name for your files. No spaces allowed, it must match SK file name you use.
+echo REM Always cook the SK_Vanity_Eyebrows_ for this folder and use its name for internal name.
+echo REM Example: for SK_Vanity_Eyebrows_MyEyebrows, use SET InternalEyebrowsName=MyEyebrows
+
+echo SET InternalEyebrowsName=MyEyebrows
+
+echo REM User-visible names, feel free to use spaces here
+
+echo SET UserVisibleName=My new eyebrows
+echo SET UserVisibleDesc=My new eyebrows description
+
+echo REM Generate a fairly random GUID that will be used to track purchasing and seleciton
+
+echo SET ProgressionGUID=0123456789ABCDEF0123456789ABCDEF
+
+echo REM Set desired resource costs
+
+echo SET CraftingCreditsCost=1000
+echo SET CostBismor=0
+echo SET CostMagnite=0
+echo SET CostUmanite=0
+echo SET CostEnorPearl=0
+echo SET CostJadiz=0
+echo SET CostCroppa=0
